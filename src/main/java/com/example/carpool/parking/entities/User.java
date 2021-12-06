bd71ea0186d35a9735a3b98cfbf3dc6402be2120
testpilottt <rauelca@gmail.com>
2021-12-06 12:02:07 +0400

    Create repo. RequestRepository

--- src/main/java/com/example/carpool/parking/entities/User.java
+++ src/main/java/com/example/carpool/parking/entities/User.java
@@ -10,6 +10,7 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+
 public class User {
 
     @Id
--- src/main/java/com/example/carpool/parking/repository/RequestRepository.java
+++ src/main/java/com/example/carpool/parking/repository/RequestRepository.java
@@ -5,15 +5,15 @@ import com.example.carpool.parking.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Integer> {
 
     List<Request> searchByDriverVisa(String driverVisa);
+
     List<Request> searchByUser(User user);
+
     List<Request> searchByDriverVisaAndStatus(String driverVisa, String status);
 
 }
