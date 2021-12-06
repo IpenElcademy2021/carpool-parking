7138e7367b85a8a3581d766e8671bfe066693bef
testpilottt <rauelca@gmail.com>
2021-12-06 10:58:48 +0400

    pulled then pushed

---
+++ src/main/java/com/example/carpool/parking/repository/CarpoolRequestRepository.java
@@ -0,0 +1,16 @@
+package com.example.carpool.parking.repository;
+
+import com.example.carpool.parking.entities.UserRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarpoolRequestRepository extends JpaRepository<UserRequest, Long> {
+
+    @Query("from UserRequest ur join fetch ur.pooling p where p.user.visa = :visa")
+    List<UserRequest> findUserRequestByVisa(@Param("visa")String visa);
+}
